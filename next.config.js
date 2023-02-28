509afecc34d6f1c2a815ac9139150482a55b6bd9
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-02-28 16:44:40 -0500

    remove webpack3

--- next.config.js
+++ next.config.js
@@ -8,16 +8,16 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const moduleExports = {
   // Your existing module.exports
 
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    debug:true,
-    hideSourceMaps: true,
-  },
+  // sentry: {
+  //   // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+  //   // for client-side builds. (This will be the default starting in
+  //   // `@sentry/nextjs` version 8.0.0.) See
+  //   // https://webpack.js.org/configuration/devtool/ and
+  //   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+  //   // for more information.
+  //   debug:true,
+  //   hideSourceMaps: true,
+  // },
 };
 
 // const sentryWebpackPluginOptions = {
