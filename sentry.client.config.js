3ad32630c40556c91a5a14a4226279359c70df2e
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-03-13 15:58:54 -0400

    testSha3

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -16,7 +16,7 @@ Sentry.init({
     }),
   ],
   // ...
-  //test
+  //test2222
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
