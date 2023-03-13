1ad0e0013dd6cb6bb22e01d9735f15c360413206
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-03-13 15:55:58 -0400

    test commit sha

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const sentryWebpackPluginOptions = {
 
   silent: false,
   ignore: [],
-  release: "2.2323",
+  release: process.env.VERCEL_GIT_COMMIT_SHA,
  // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
